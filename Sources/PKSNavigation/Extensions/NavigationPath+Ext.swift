4ea34fa7e7df7ed745c95a54153a6e3b6c3c89f2
Omer Hamid Kamisli <me@ohkamisli.dev>
2024-07-02 00:29:56 -0700

    feat: Add NavigationPath extension methods for removing last element and clearing the path

---
+++ Sources/PKSNavigation/Extensions/NavigationPath+Ext.swift
@@ -0,0 +1,43 @@
+//
+//  NavigationPath+Ext.swift
+//
+//
+//  Created by Ömer Hamid Kamışlı on 7/1/24.
+//
+
+import SwiftUI
+
+/// An extension that provides additional utility methods for `NavigationPath`.
+///
+/// This extension adds methods to safely remove the last element of the path
+/// and to clear all elements in the path.
+///
+/// Example:
+///
+///     var path = NavigationPath()
+///     path.append("FirstPage")
+///     path.removeLastIfAvailable() // Removes "FirstPage" if it exists
+///     path.clear() // Clears the entire path
+///
+extension NavigationPath {
+
+    /// Removes the last element of the path if it exists.
+    ///
+    /// Use this method to safely remove the last element of the `NavigationPath`
+    /// without causing an out-of-bounds error.
+    mutating func removeLastIfAvailable() {
+        if !self.isEmpty {
+            self.removeLast()
+        }
+    }
+    
+    /// Clears all elements in the path.
+    ///
+    /// Use this method to remove all elements from the `NavigationPath`,
+    /// effectively resetting the path.
+    mutating func clear() {
+        if !self.isEmpty {
+            self.removeLast(self.count)
+        }
+    }
+}
