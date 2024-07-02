236a2f871c1f349036f2e9b2be47114377beec4a
Omer Hamid Kamisli <me@ohkamisli.dev>
2024-07-02 00:29:46 -0700

    feat: Add PKSPresentationMethod enum for page presentation methods

---
+++ Sources/PKSNavigation/Enums/PKSPresentationMethod.swift
@@ -0,0 +1,34 @@
+//
+//  PKSPresentationMethod.swift
+//
+//
+//  Created by Ömer Hamid Kamışlı on 7/1/24.
+//
+
+import Foundation
+
+/// An enumeration that defines the different presentation methods for displaying a page.
+///
+/// Use `PKSPresentationMethod` to specify how a page should be presented
+/// within your app. The enumeration supports three methods: stack, sheet, and cover.
+///
+/// Example:
+///
+///     let method: PKSPresentationMethod = .stack
+///
+/// - stack: The page is presented in a stack.
+/// - sheet: The page is presented as a sheet.
+/// - cover: The page is presented in fullscreen.
+///
+/// Each case is associated with a string value for encoding and decoding purposes.
+public enum PKSPresentationMethod: String, Codable {
+    /// The page is presented in a stack.
+    case stack = "STACK"
+    
+    /// The page is presented as a sheet.
+    case sheet = "SHEET"
+    
+    /// The page is presented in fullscreen.
+    case cover = "FULLSCREEN"
+}
+
