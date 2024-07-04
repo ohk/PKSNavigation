ebcb5eeb000bb399de05e849fc5f9c296fcaef09
Omer Hamid Kamisli <me@ohkamisli.dev>
2024-07-04 13:10:59 -0700

    feat: Add PKSHistoryItem struct for representing navigation history
    This commit adds a new file `PKSHistoryItem.swift` which contains the `PKSHistoryItem` struct. This struct represents a navigation history item and includes properties for the page, presentation method, timestamp, message, and other relevant information. It conforms to the `Identifiable`, `Equatable`, and `Hashable` protocols. The `PKSHistoryItem` struct enhances the navigation functionality by providing a structured representation of navigation events.

---
+++ Sources/PKSNavigation/Models/PKSHistoryItem.swift
@@ -0,0 +1,46 @@
+//
+//  PKSHistoryItem.swift
+//  
+//
+//  Created by Ömer Hamid Kamışlı on 7/4/24.
+//
+
+import Foundation
+
+/// A struct that represents a navigation history item.
+struct PKSHistoryItem: Identifiable, Equatable, Hashable {
+    // Identifiable
+    var id: UUID = UUID()
+
+    // The page that was navigated to.
+    var page: (any PKSPage)? = nil
+
+    // The presentation method used to navigate to the page.
+    var presentation: PKSPresentationMethod? = nil
+
+    // The timestamp of the navigation event.
+    var timestamp: Date = .now
+
+    // The message for the navigation event.
+    var message: String? = nil
+
+    // Whether the item is appended to the history.
+    var isAppended: Bool = false
+
+    // Whether the navigation is handled by a parent.
+    var isParent: Bool = false
+
+    // MARK: - Equatable
+    static func == (lhs: PKSHistoryItem, rhs: PKSHistoryItem) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    // MARK: - Hashable
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(presentation)
+        hasher.combine(timestamp)
+        hasher.combine(message)
+        hasher.combine(isAppended)
+    }
+}
