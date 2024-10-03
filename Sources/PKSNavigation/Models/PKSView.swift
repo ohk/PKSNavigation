ddaf45eee122c3f68fa65578f141cf8a528cc39d
Omer Hamid Kamisli <ohkamisli@poikus.com>
2024-10-02 18:41:07 -0700

    feat: Add copyright notice to PKSView.swift

--- Sources/PKSNavigation/Models/PKSView.swift
+++ Sources/PKSNavigation/Models/PKSView.swift
@@ -1,9 +1,9 @@
 //
 //  PKSView.swift
-//  
+//
 //
 //  Created by Ömer Hamid Kamışlı on 7/1/24.
-//
+//  Copyright © 2024 POIKUS LLC. All rights reserved.
 
 import SwiftUI
 
@@ -23,19 +23,19 @@ import SwiftUI
 ///     }
 ///
 public struct PKSView: Identifiable, Equatable, Hashable {
-    
+
     // MARK: - Identifiable Conformance
-    
+
     /// A unique identifier for the root view, derived from the wrapped view's ID.
     public var id: Int { wrapped.id }
-    
+
     // MARK: - Properties
-    
+
     /// The wrapped view conforming to the `PKSPage` protocol.
     var wrapped: any PKSPage
-    
+
     // MARK: - Equatable Conformance
-    
+
     /// Compares two `RootView` instances for equality based on their wrapped view's ID.
     ///
     /// - Parameters:
@@ -45,25 +45,24 @@ public struct PKSView: Identifiable, Equatable, Hashable {
     public static func == (lhs: PKSView, rhs: PKSView) -> Bool {
         rhs.wrapped.id == lhs.wrapped.id
     }
-    
+
     // MARK: - Hashable Conformance
-    
+
     /// Hashes the essential components of the `RootView` by combining the wrapped view.
     ///
     /// - Parameter hasher: The hasher to use when combining the components.
     public func hash(into hasher: inout Hasher) {
         hasher.combine(wrapped)
     }
-    
+
     // MARK: - View
-    
+
     /// The body of the `RootView`, providing the content to display.
     ///
     /// This computed property returns the wrapped view if it can be cast to `AnyView`.
     /// Otherwise, it displays a fallback message indicating an issue.
     @MainActor @ViewBuilder
     public var view: some View {
-       AnyView(wrapped.body)
+        AnyView(wrapped.body)
     }
 }
-
